5c39a4b8ab1a0fa0b1cad0d26a6841fc38c0b844
Vladimir Samojlov <v-moi@ya.ru>
2018-09-11 00:53:00 +0300

    edit single

--- single.php
+++ single.php
@@ -1,12 +1,30 @@
 <?php get_header(); ?>
-<section class="entry-section">
-
-    <?php
-    if( have_posts() ): the_post();
-        get_template_part('blocks/default/content', 'default');
-    endif;
-    ?>
-
-	<?php wp_link_pages(); ?>
-</section>
+<div class="single bg-light py-5">
+	<div class="container">
+		<div class="row">
+			<?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
+				<div class="col-md-8">
+					<div class="box p-4">
+						<div class="single__title mb-3">
+							<?php the_title(); ?>
+						</div>
+						<div class="single__thumb mb-3">
+							<img src="<?php echo get_the_post_thumbnail_url() ?>" alt="">
+						</div>
+						<div class="single__content">
+							<?php the_content(); ?>
+						</div>
+					</div>
+				</div>
+				<div class="col-md-4">
+					<div class="box p-4">
+						Сайдбар	
+					</div>
+				</div>
+			<?php endwhile; else: ?>
+				<p><?php _e('Sorry, no posts matched your criteria.'); ?></p>
+			<?php endif; ?>
+		</div>
+	</div>
+</div>
 <?php get_footer(); ?>
