3c9813857127504f33713519f4091c4ce246be07
Vladimir Samojlov <v-moi@ya.ru>
2018-09-10 23:41:26 +0300

    edit tpl_news

--- tpl_news.php
+++ tpl_news.php
@@ -9,19 +9,51 @@ Template Name: Новости
 	<?php get_template_part( 'blocks/hero', 'default' ); ?>
 <!-- end hero -->
 <div class="news bg-light py-5">
+	<div class="container mb-5">
+		<div class="topnews">
+			<div class="row">
+				<?php
+					$custom_query_news = new WP_Query( array( 'post_type' => 'post', 'posts_per_page' => 1) );
+					if( $custom_query_news->have_posts() ){
+						while( $custom_query_news->have_posts() ){ $custom_query_news->the_post();
+				?>
+					<div class="col-md-8">
+						<div class="topnews__photo news__img">
+							<img src="<?php echo get_the_post_thumbnail_url() ?>" alt="<?php the_title(); ?>">
+						</div>
+					</div>
+					<div class="col-md-4 topnews__border d-flex align-items-center justify-content-center">
+						<div class="topnews__info">
+							<div class="news__category">
+								<?php echo the_category() ?>	
+							</div>
+							<div class="news__title">
+								<a href="<?php the_permalink(); ?>"><?php the_title(); ?></a>
+							</div>
+						</div>	
+					</div>
+				<?php
+					}
+						wp_reset_postdata(); // сбрасываем переменную $post
+					} 
+					else echo 'Записей нет2.';
+				?>	
+			</div>
+		</div>
+	</div>
 	<div class="container">
 		<div class="row">
 			<div class="col-md-6">
 				<?php
-					$custom_query_news = new WP_Query( array( 'post_type' => 'post' ) );
+					$custom_query_news = new WP_Query( array( 'post_type' => 'post', 'offset'=> 1 ) );
 					if( $custom_query_news->have_posts() ){
 						while( $custom_query_news->have_posts() ){ $custom_query_news->the_post();
 				?>
 					<div class="news__item">
 						<div class="news__img">
-							<?php echo get_the_post_thumbnail_url() ?>
+							<img src="<?php echo get_the_post_thumbnail_url() ?>" alt="<?php the_title(); ?>">
 						</div>
-						<div class="news__info">
+						<div class="news__info py-5">
 							<div class="news__category">
 								<?php echo the_category() ?>	
 							</div>
@@ -39,6 +71,7 @@ Template Name: Новости
 			</div>
 		</div>
 	</div>
+
 </div>
 
 
