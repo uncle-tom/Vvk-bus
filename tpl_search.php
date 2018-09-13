04c3b8e1469254e50f0226138631539080c86a73
Vladimir Samojlov <v-moi@ya.ru>
2018-09-13 14:00:13 +0300

    edit search tpl

--- tpl_search.php
+++ tpl_search.php
@@ -12,7 +12,9 @@ Template Name: Маршрут
 <div class="container py-5">
 	<div class="row">
 		<div class="col-md-12">
-			<link rel="stylesheet" href="https://octobus.cloud/frameapp/css/app.css" media="all"> <link rel="stylesheet" href="https://octobus.cloud/frameapp/print.css" media="print"> <div id="app" data-from="207" data-to="11" data-spa="0" data-ptoken="76a04d54ec444983b4f91ca3ac12f114" data-lang="uk" data-env="prod"></div> <script src="https://octobus.cloud/frameapp/js/manifest.js"></script> <script src="https://octobus.cloud/frameapp/js/vendor.js"></script> <script src="https://octobus.cloud/frameapp/js/app.js"></script>			
+			<div class="route">
+				<link rel="stylesheet" href="https://octobus.cloud/frameapp/css/app.css" media="all"> <link rel="stylesheet" href="https://octobus.cloud/frameapp/print.css" media="print"> <div id="app" data-from="207" data-to="11" data-spa="0" data-ptoken="76a04d54ec444983b4f91ca3ac12f114" data-lang="uk" data-env="prod"></div> <script src="https://octobus.cloud/frameapp/js/manifest.js"></script> <script src="https://octobus.cloud/frameapp/js/vendor.js"></script> <script src="https://octobus.cloud/frameapp/js/app.js"></script>
+			</div>
 		</div>
 	</div>
 </div>
