0a4dc98b44125aa221f5d3940204f86374dbb5ee
Vladimir Samojlov <v-moi@ya.ru>
2018-09-13 07:35:03 +0300

    change url for logo in login form

--- functions.php
+++ functions.php
@@ -213,6 +213,16 @@ function theme_settings_page() {
     include 'form-file.php';
 }
 
+function my_login_logo_url() {
+    return home_url();
+}
+add_filter( 'login_headerurl', 'my_login_logo_url' );
+
+function my_login_logo_url_title() {
+    return 'Vvk-bus';
+}
+add_filter( 'login_headertitle', 'my_login_logo_url_title' );
+
 function my_login_logo() { ?>
     <style type="text/css">
         #login h1 a, .login h1 a {
