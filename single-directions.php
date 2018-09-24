ea145b0cc100d55c8a3c43cf69e404c666f6156a
Vladimir Samojlov <v-moi@ya.ru>
2018-09-24 19:58:37 +0300

    edit single directions

--- single-directions.php
+++ single-directions.php
@@ -3,15 +3,15 @@
 <!-- begin hero -->
 	<?php get_template_part( 'blocks/hero', 'default' ); ?>
 <!-- end hero -->
-<div class="container py-5">
+<div class="container-fluid py-5">
 	<!-- begin BOOKING -->
 	<div class="row">
 		<div class="col-md-12">
-			<div class="booking">
-				<link rel="stylesheet" href="https://octobus.cloud/frameapp/css/app.css" media="all"> <link rel="stylesheet" href="https://octobus.cloud/frameapp/print.css" media="print"> <div id="app" data-spa="1" data-ptoken="76a04d54ec444983b4f91ca3ac12f114" data-lang="uk" data-env="prod"></div> <script src="https://octobus.cloud/frameapp/js/manifest.js"></script> <script src="https://octobus.cloud/frameapp/js/vendor.js"></script> <script src="https://octobus.cloud/frameapp/js/app.js"></script>
-			</div>			
+			<link rel="stylesheet" href="https://octobus.cloud/frameapp/css/app.css" media="all"> <link rel="stylesheet" href="https://octobus.cloud/frameapp/print.css" media="print"> <div id="app" data-spa="1" data-ptoken="76a04d54ec444983b4f91ca3ac12f114" data-lang="uk" data-env="prod"></div> <script src="https://octobus.cloud/frameapp/js/manifest.js"></script> <script src="https://octobus.cloud/frameapp/js/vendor.js"></script> <script src="https://octobus.cloud/frameapp/js/app.js"></script>
 		</div>
 	</div>
+</div>
+<div class="container">
 	<!-- end BOOKING -->
 	<?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
 	<div class="row">
