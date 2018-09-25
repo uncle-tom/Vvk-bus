34a1a3b2125a2a4fc50887ed22e29e267569de5d
Vladimir Samojlov <v-moi@ya.ru>
2018-09-25 13:58:57 +0300

    dir lines

--- blocks/dir-lines.php
+++ blocks/dir-lines.php
@@ -1,14 +1,14 @@
 <div class="col-md-12">
-	<div class="khpoz dirlines">
-		<div class="khpoz__kh dirline"><span></span>Харьков</div>
-		<div class="khpoz__poltava dirline">Полтава</div>
-		<div class="khpoz__kiyv dirline">Киев</div>
-		<div class="khpoz__jitomir dirline">Житомир</div>
-		<div class="khpoz__rovno dirline">Ровно</div>
-		<div class="khpoz__lviv dirline">Львов</div>
-		<div class="khpoz__krakov dirline">Краков</div>
-		<div class="khpoz__katovice dirline">Катовице</div>
-		<div class="khpoz__vrozlav dirline">Вроцлав</div>
-		<div class="khpoz__poznan dirline">Познань</div>
+	<div class="khpoz dirlines pt-5">
+		<div class="khpoz__kh dirline"><span class="dirline__name">Харьков</span><span class="dirline__circle"></span></div>
+		<div class="khpoz__poltava dirline"><span class="dirline__name">Полтава</span><span class="dirline__circle"></span></div>
+		<div class="khpoz__kiyv dirline"><span class="dirline__name">Киев</span><span class="dirline__circle"></span></div>
+		<div class="khpoz__jitomir dirline"><span class="dirline__name">Житомир</span><span class="dirline__circle"></span></div>
+		<div class="khpoz__rovno dirline"><span class="dirline__name">Ровно</span><span class="dirline__circle"></span></div>
+		<div class="khpoz__lviv dirline"><span class="dirline__name">Львов</span><span class="dirline__circle"></span></div>
+		<div class="khpoz__krakov dirline"><span class="dirline__name">Краков</span><span class="dirline__circle"></span></div>
+		<div class="khpoz__katovice dirline"><span class="dirline__name">Катовице</span><span class="dirline__circle"></span></div>
+		<div class="khpoz__vrozlav dirline"><span class="dirline__name">Вроцлав</span><span class="dirline__circle"></span></div>
+		<div class="khpoz__poznan dirline"><span class="dirline__name">Познань</span><span class="dirline__circle"></span></div>
 	</div>
 </div>
