63b2a72fc347b365fbb75a802a513ea3aaa833a0
Vladimir Samojlov <v-moi@ya.ru>
2018-09-25 18:08:35 +0300

    dir lines change style and clases

--- blocks/dir-lines.php
+++ blocks/dir-lines.php
@@ -1,14 +1,36 @@
 <div class="col-md-12">
-	<div class="khpoz dirlines pt-5">
-		<div class="khpoz__kh dirline"><span class="dirline__name">Харьков</span><span class="dirline__circle"></span></div>
-		<div class="khpoz__poltava dirline"><span class="dirline__name">Полтава</span><span class="dirline__circle"></span></div>
-		<div class="khpoz__kiyv dirline"><span class="dirline__name">Киев</span><span class="dirline__circle"></span></div>
-		<div class="khpoz__jitomir dirline"><span class="dirline__name">Житомир</span><span class="dirline__circle"></span></div>
-		<div class="khpoz__rovno dirline"><span class="dirline__name">Ровно</span><span class="dirline__circle"></span></div>
-		<div class="khpoz__lviv dirline"><span class="dirline__name">Львов</span><span class="dirline__circle"></span></div>
-		<div class="khpoz__krakov dirline"><span class="dirline__name">Краков</span><span class="dirline__circle"></span></div>
-		<div class="khpoz__katovice dirline"><span class="dirline__name">Катовице</span><span class="dirline__circle"></span></div>
-		<div class="khpoz__vrozlav dirline"><span class="dirline__name">Вроцлав</span><span class="dirline__circle"></span></div>
-		<div class="khpoz__poznan dirline"><span class="dirline__name">Познань</span><span class="dirline__circle"></span></div>
+	<div class="dirlines red py-5">
+		<div class="dirline"><span class="dirline__name">Полтава</span><span class="dirline__circle"></span></div>
+		<div class="dirline"><span class="dirline__name">Харьков</span><span class="dirline__circle"></span></div>
+		<div class="dirline"><span class="dirline__name">Курск</span><span class="dirline__circle"></span></div>
+		<div class="dirline"><span class="dirline__name">Москва</span><span class="dirline__circle"></span></div>
+	</div>
+	<div class="dirlines yellow py-5">
+		<div class="dirline"><span class="dirline__name">Харьков</span><span class="dirline__circle"></span></div>
+		<div class="dirline"><span class="dirline__name">Полтава</span><span class="dirline__circle"></span></div>
+		<div class="dirline"><span class="dirline__name">Киев</span><span class="dirline__circle"></span></div>
+		<div class="dirline"><span class="dirline__name">Житомир</span><span class="dirline__circle"></span></div>
+		<div class="dirline"><span class="dirline__name">Ровно</span><span class="dirline__circle"></span></div>
+		<div class="dirline"><span class="dirline__name">Львов</span><span class="dirline__circle"></span></div>
+		<div class="dirline"><span class="dirline__name">Краков</span><span class="dirline__circle"></span></div>
+		<div class="dirline"><span class="dirline__name">Катовице</span><span class="dirline__circle"></span></div>
+		<div class="dirline"><span class="dirline__name">Вроцлав</span><span class="dirline__circle"></span></div>
+		<div class="dirline"><span class="dirline__name">Познань</span><span class="dirline__circle"></span></div>
+	</div>
+	<div class="dirlines green py-5">
+		<div class="dirline"><span class="dirline__name">Харьков</span><span class="dirline__circle"></span></div>
+		<div class="dirline"><span class="dirline__name">Полтава</span><span class="dirline__circle"></span></div>
+		<div class="dirline"><span class="dirline__name">Киев</span><span class="dirline__circle"></span></div>
+		<div class="dirline"><span class="dirline__name">Житомир</span><span class="dirline__circle"></span></div>
+		<div class="dirline"><span class="dirline__name">Ровно</span><span class="dirline__circle"></span></div>
+		<div class="dirline"><span class="dirline__name">Луцк</span><span class="dirline__circle"></span></div>
+		<div class="dirline"><span class="dirline__name">Люблин</span><span class="dirline__circle"></span></div>
+		<div class="dirline"><span class="dirline__name">Варшава</span><span class="dirline__circle"></span></div>
+		<div class="dirline"><span class="dirline__name">Лодзь</span><span class="dirline__circle"></span></div>
+		<div class="dirline"><span class="dirline__name">Влоцлавек</span><span class="dirline__circle"></span></div>
+		<div class="dirline"><span class="dirline__name">Торунь</span><span class="dirline__circle"></span></div>
+		<div class="dirline"><span class="dirline__name">Быдгощ</span><span class="dirline__circle"></span></div>
+		<div class="dirline"><span class="dirline__name">Гданьск</span><span class="dirline__circle"></span></div>
+		<div class="dirline"><span class="dirline__name">Гдыня</span><span class="dirline__circle"></span></div>
 	</div>
 </div>
