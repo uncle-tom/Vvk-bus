dd5e35f3cebbb017f33090168ec88e01b30a29c9
Vladimir Samojlov <v-moi@ya.ru>
2018-09-27 10:39:39 +0300

    add main tpl

---
+++ tpl_main.php
@@ -0,0 +1,14 @@
+<?php
+/*
+Template Name: Главная
+*/
+?>
+
+<?php get_header(); ?>
+<section class="entry-section">
+	<?php get_template_part( 'blocks/welcome', 'default' ); ?>
+	<?php get_template_part( 'blocks/directions', 'default' ); ?>
+	<?php get_template_part( 'blocks/whyus', 'default' ); ?>
+	<?php get_template_part( 'blocks/about', 'default' ); ?>
+</section>
+<?php get_footer(); ?>
