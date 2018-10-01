4ac7eb044bbb59b3dcf5afe2a85763a68ed4b5d2
Vladimir Samojlov <v-moi@ya.ru>
2018-10-01 23:07:10 +0300

    bus animation

--- blocks/dir-lines.php
+++ blocks/dir-lines.php
@@ -1,12 +1,18 @@
 <div class="col-md-12">
 	<div class="dirlines red py-5">
-		<div class="dirline"><span class="dirline__name">Полтава</span><span class="dirline__circle"></span></div>
+
+		<div class="dirline"><span class="dirline__name">Полтава</span><span class="dirline__circle"></span>
+			<img src="http://vvkbus.wemodern.com/wp-content/uploads/2018/09/bus-icon-1.png" alt="" class="dirline__bus">
+		</div>
 		<div class="dirline"><span class="dirline__name">Харьков</span><span class="dirline__circle"></span></div>
 		<div class="dirline"><span class="dirline__name">Курск</span><span class="dirline__circle"></span></div>
 		<div class="dirline"><span class="dirline__name">Москва</span><span class="dirline__circle"></span></div>
+
 	</div>
 	<div class="dirlines yellow py-5">
-		<div class="dirline"><span class="dirline__name">Харьков</span><span class="dirline__circle"></span></div>
+		<div class="dirline"><span class="dirline__name">Харьков</span><span class="dirline__circle"></span>
+			<img src="http://vvkbus.wemodern.com/wp-content/uploads/2018/09/bus-icon-1.png" alt="" class="dirline__bus">
+		</div>
 		<div class="dirline"><span class="dirline__name">Полтава</span><span class="dirline__circle"></span></div>
 		<div class="dirline"><span class="dirline__name">Киев</span><span class="dirline__circle"></span></div>
 		<div class="dirline"><span class="dirline__name">Житомир</span><span class="dirline__circle"></span></div>
@@ -18,7 +24,9 @@
 		<div class="dirline"><span class="dirline__name">Познань</span><span class="dirline__circle"></span></div>
 	</div>
 	<div class="dirlines green py-5">
-		<div class="dirline"><span class="dirline__name">Харьков</span><span class="dirline__circle"></span></div>
+		<div class="dirline"><span class="dirline__name">Харьков</span><span class="dirline__circle"></span>
+			<img src="http://vvkbus.wemodern.com/wp-content/uploads/2018/09/bus-icon-1.png" alt="" class="dirline__bus">
+		</div>
 		<div class="dirline"><span class="dirline__name">Полтава</span><span class="dirline__circle"></span></div>
 		<div class="dirline"><span class="dirline__name">Киев</span><span class="dirline__circle"></span></div>
 		<div class="dirline"><span class="dirline__name">Житомир</span><span class="dirline__circle"></span></div>
