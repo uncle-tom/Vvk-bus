ea00960f84bc22a76a52b4570a7edbc0792150ea
Vladimir Samojlov <v-moi@ya.ru>
2019-01-29 13:37:31 +0200

    edit octobus code on another page

--- single-directions.php
+++ single-directions.php
@@ -7,7 +7,7 @@
 	<!-- begin BOOKING -->
 	<div class="row">
 		<div class="col-md-12">
-			<link rel="stylesheet" href="https://octobus.cloud/frameapp/css/app.css" media="all"> <link rel="stylesheet" href="https://octobus.cloud/frameapp/print.css" media="print"> <div id="app" data-spa="1" data-ptoken="76a04d54ec444983b4f91ca3ac12f114" data-lang="uk" data-env="prod"></div> <script src="https://octobus.cloud/frameapp/js/manifest.js"></script> <script src="https://octobus.cloud/frameapp/js/vendor.js"></script> <script src="https://octobus.cloud/frameapp/js/app.js"></script>
+			<div id="auth" class="noprint"></div><div id="app" data-spa="1" data-ptoken="76a04d54ec444983b4f91ca3ac12f114" data-lang="ru" data-env="prod" ></div>
 		</div>
 	</div>
 </div>
